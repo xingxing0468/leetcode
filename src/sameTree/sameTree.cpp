99f5a0b371ea1529aa511d47deb7a2e3af8c6033
zZED <xingxing0468@hotmail.com>
2014-11-10 10:51:49 +0800

    overlap demeo

--- src/sameTree/sameTree.cpp
+++ src/sameTree/sameTree.cpp
@@ -26,10 +26,28 @@ public:
         srand(time(NULL));
     }
     bool isSameTree(TreeNode *p, TreeNode *q) {
-        if (random()%2){
-            return isSameTree1(p, q);
+        queue<TreeNode*> q1, q2;
+        q1.push(p);
+        q2.push(q);
+        while (q1.size()>0 && q2.size()>0 ){
+            TreeNode* p1 = q1.front();
+            q1.pop();
+            TreeNode* p2 = q2.front();
+            q2.pop();
+            if (!p1 && !p2) continue;
+            if (!p1 || !p2)  return false;
+
+            if ( p1->val != p2->val) {
+                return false;
+            }
+
+            q1.push(p1->left);
+            q2.push(p2->left);
+
+            q1.push(p1->right);
+            q2.push(p2->right);
+
         }
-        return isSameTree2(p, q);
     }
     
     bool isSameTree1(TreeNode *p, TreeNode *q) {
